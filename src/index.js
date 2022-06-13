fe89904529094803bd3a7ef04946479081364315
mejidong <dowcc@naver.com>
2022-06-13 11:23:57 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,30 @@
-let maxNum = document.querySelector("#maxNumber");
-let choseNum = document.querySelector("#choseNumber");
+const maxNum = document.querySelector("#maxNumber");
+const choseNum = document.querySelector("#choseNumber");
 const gameForm = document.querySelector("#game");
-
-const result = document.querySelector("#GameResult");
 
 const compareResult = document.querySelector("#compareNumber");
 const allResult = document.querySelector("#gameResult");
 
 function submitNum(event) {
   event.preventDefault();
-  const newMaxnum = maxNum.value;
-  const newChosenum = choseNum.value;
-  const intMax = parseInt(newMaxnum);
-  const intChose = parseInt(newChosenum);
+  let newMaxnum = maxNum.value;
+  let newChosenum = choseNum.value;
+
   if (newMaxnum === "" || newChosenum === "") {
     alert("Please enter all number.");
-  } else if (intMax < intChose) {
+  } else if (newMaxnum < newChosenum) {
     alert("Maximum number cannot be less than the chose number.");
   } else {
-    getResult(intMax, intChose);
+    getResult(newMaxnum, newChosenum);
   }
 }
 
 function getResult(maxnum, chosenum) {
   let machineNum = Math.ceil(Math.random() * (parseInt(maxnum) + 1) - 1);
   compareResult.innerText = `You chose: ${chosenum}, the machine chose: ${machineNum}.`;
-  if (parseInt(chosenum) === machineNum) {
+  if (chosenum === machineNum) {
     allResult.innerText = `You won!`;
-  } else if (parseInt(chosenum) !== machineNum) {
+  } else if (chosenum !== machineNum) {
     allResult.innerText = `You lost!`;
   }
 }
